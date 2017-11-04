38ea5590ebc6115d054767f0a7da176868330377
John <jcobb00@users.noreply.github.com>
2017-11-03 22:53:51 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -10,6 +10,15 @@ if(!"tidyr" %in% rownames(installed.packages())) {install.packages("tidyr")}
 #load tidyr
 if(!"tidyr" %in% rownames(.packages())) {library(tidyr)}
 
+#install lubridate
+if(!"lubridate" %in% rownames(installed.packages())) {install.packages("lubridate")}
+
+#load lubridate
+if(!"lubridate" %in% rownames(.packages())) {library(lubridate)}
+
+#install files to
+getwd()
+
 #create directory
 if(!file.exists("./data/power")) {dir.create("./data/power")}
 
@@ -23,7 +32,61 @@ if(!file.exists("./data/power/household_power_consumption.txt")){unzip("./data/p
 file.size("./data/power/household_power_consumption.txt")/1073741824
 
 #load table
-data <- read.table("./data/power/household_power_consumption.txt", header = TRUE,sep=";",nrows=10000)
+data <- read.table("./data/power/household_power_consumption.txt", header = TRUE,sep=";", na.strings = "?", skipNul = TRUE )
 
-1+1
+#format date and time
+data$Time <- as.POSIXct(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S")
 
+#format date
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+
+#filter dates
+powerdata <- filter(data, Date == as.Date('2007-02-01') | Date == as.Date('2007-02-02'))
+
+#open png
+png(file = "plot1.png")
+
+#plot 1
+hist(powerdata$Global_active_power, col = "Red", xlab = "Global Active Power (kilowatts)",main = "Global Active Power")
+
+#close png
+dev.off()
+
+#open png
+png(file = "plot2.png")
+
+#plot 1
+plot(powerdata$Time ,powerdata$Global_active_power, type = "l", xlab = '', ylab = "Global Active Power (kilowatts)")
+
+#close png
+dev.off()
+
+#open png
+png(file = "plot3.png")
+
+#plot 2
+plot(powerdata$Time,  powerdata$Sub_metering_1, type = "l", xlab = '', ylab = "Energy sub metering")
+  lines(powerdata$Time, powerdata$Sub_metering_2, col="Red")
+  lines(powerdata$Time, powerdata$Sub_metering_3, col="Blue")
+  legend("topright",lty=1,col = c("black","red","blue"),legend=(c("Sub_metering_1","Sub_metering_2","Sub_metering_3")))
+  
+#close png
+dev.off()
+
+#open png
+png(file = "plot4.png", length = 480, width = 480)
+
+#plot 4
+par(mfrow = c(2,2),lwd = .1)
+with(powerdata, {
+  plot(Time, Global_active_power, type = "l", xlab = '', ylab = "Global Active Power")
+  plot(Time, Voltage, type = "l", xlab = 'datetime', ylab = "Voltage")
+plot(powerdata$Time,  powerdata$Sub_metering_1, type = "l", xlab = '', ylab = "Energy sub metering")
+  lines(powerdata$Time, powerdata$Sub_metering_2, col="Red")
+  lines(powerdata$Time, powerdata$Sub_metering_3, col="Blue")
+  legend("topright",lty=1,col = c("black","red","blue"),bty = "n",legend=(c("Sub_metering_1","Sub_metering_2","Sub_metering_3")))
+  plot(Time, Global_reactive_power, type = "l", xlab = 'datetime', ylab = "Global_reactive_power")
+})
+
+#close png
+dev.off()
