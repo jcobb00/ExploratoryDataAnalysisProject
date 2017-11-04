881b92b46598ec4c8cf647685d78315461238ea0
John <jcobb00@users.noreply.github.com>
2017-11-03 23:59:41 -0400

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -41,19 +41,19 @@ data$Time <- as.POSIXct(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S")
 data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
 
 #filter dates
-powerdata <- filter(data, Date == as.Date('2007-02-01') | Date == as.Date('2007-02-02'))
+data <- filter(data, Date == as.Date('2007-02-01') | Date == as.Date('2007-02-02'))
 
 #open plot 4
-png(file = "plot4.png", length = 480, width = 480)
+png(file = "plot4.png")
 
 #plot 4
 par(mfrow = c(2,2),lwd = .1)
-with(powerdata, {
+with(data, {
   plot(Time, Global_active_power, type = "l", xlab = '', ylab = "Global Active Power")
   plot(Time, Voltage, type = "l", xlab = 'datetime', ylab = "Voltage")
-plot(powerdata$Time,  powerdata$Sub_metering_1, type = "l", xlab = '', ylab = "Energy sub metering")
-  lines(powerdata$Time, powerdata$Sub_metering_2, col="Red")
-  lines(powerdata$Time, powerdata$Sub_metering_3, col="Blue")
+plot(data$Time,  data$Sub_metering_1, type = "l", xlab = '', ylab = "Energy sub metering")
+  lines(data$Time, data$Sub_metering_2, col="Red")
+  lines(data$Time, data$Sub_metering_3, col="Blue")
   legend("topright",lty=1,col = c("black","red","blue"),bty = "n",legend=(c("Sub_metering_1","Sub_metering_2","Sub_metering_3")))
   plot(Time, Global_reactive_power, type = "l", xlab = 'datetime', ylab = "Global_reactive_power")
 })
